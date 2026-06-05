3972e57d0843ace72d180e77c4e6d19838a36477
Michael Docentes <mdocentes@academic.rrc.ca>
2026-06-04 21:41:21 -0500

    added service tests with mocked firestore

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,10 +1,13 @@
 import { initializeApp, getApps } from "firebase/app";
-import { getFirestore} from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "your-api-key",
-    authDomain: "your-auth-domain",
-    projectId: "your-project-id",
+    apiKey: "AIzaSyBOobghHJ0eiuTB3rTXWa29cn4K-vMzHu8",
+    authDomain: "module-3-33f91.firebaseapp.com",
+    projectId: "module-3-33f91",
+    storageBucket: "module-3-33f91.firebasestorage.app",
+    messagingSenderId: "986555579962",
+    appId: "1:986555579962:web:a36c9ab8452ce6ebc97f03",
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
